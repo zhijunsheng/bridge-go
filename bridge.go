25d105c71c8e9b9eebb227b951a3793b6e00eae6
Golden Thumb <zhijunsheng@mbp2020.local>
2020-12-18 15:34:25 -0500

    implemented bridging 2 chess clients

--- bridge.go
+++ bridge.go
@@ -4,24 +4,18 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"time"
 )
 
 func main() {
 	listener, _ := net.Listen("tcp", ":50000")
 	fmt.Println("socket server listeninng on port 50000")
-	for {
-		conn, _ := listener.Accept()
-		fmt.Printf("%s <---> %s\n", conn.LocalAddr(), conn.RemoteAddr())
-		go handleConn(conn)
-	}
-}
 
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-	for col := 0; col < 5; col++ {
-		move := fmt.Sprintf("%d,1,%d,3\n", col, col)
-		io.WriteString(conn, move)
-		time.Sleep(2 * time.Second)
-	}
+	player1, _ := listener.Accept()
+	fmt.Printf("player1: %s\n", player1.RemoteAddr())
+
+	player2, _ := listener.Accept()
+	fmt.Printf("player2: %s\n", player2.RemoteAddr())
+
+	go io.Copy(player2, player1)
+	io.Copy(player1, player2)
 }
