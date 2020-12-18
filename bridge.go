8d66f567e400de4309f40f2ab49fef43f2121b4e
Golden Thumb <zhijunsheng@mbp2020.local>
2020-12-17 19:33:18 -0500

    implemented a socket server servicing a single chess move

--- bridge.go
+++ bridge.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, Bridge!")
+	listener, _ := net.Listen("tcp", ":50000")
+	fmt.Println("socket server listeninng on port 50000")
+	for {
+		conn, _ := listener.Accept()
+		fmt.Printf("%s <---> %s\n", conn.LocalAddr(), conn.RemoteAddr())
+		io.WriteString(conn, `0,0,3,3`+"\n") // 0,0,3,3\n
+	}
 }
